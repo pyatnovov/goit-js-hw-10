1a91717e51911cad870278986d3f1b33e1ceed94
Vova2580 <109892815+pyatnovov@users.noreply.github.com>
2023-01-10 16:59:49 +0200

    NPM залежності проєкту

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import './css/styles.css';
 import { fetchCountries } from './fetchCountries';
-    
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+
+const debounce = require('lodash.debounce');
+const DEBOUNCE_DELAY = 300;
 
 const refs = {
     searchInput: document.querySelector('[id=search-box]'),
 };
 
-const DEBOUNCE_DELAY = 300;
 
-console.log(refs.searchInput);
-fetchCountries();
+refs.searchInput.addEventListener('input', debounce(fetchCountries, DEBOUNCE_DELAY));
+
+
+
 
