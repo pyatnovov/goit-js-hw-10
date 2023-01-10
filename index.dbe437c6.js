d8aa80ed296fcda1f5d0f514988aaaf68263a358
pyatnovov <109892815+pyatnovov@users.noreply.github.com>
2023-01-10 14:15:26 +0000

    Deploying to gh-pages from @ pyatnovov/goit-js-hw-10@9bb6086b74b92c796b5375b7e2fef7bf3d676dc8 🚀

---
+++ index.dbe437c6.js
@@ -0,0 +1,2 @@
+!function(){var e={searchInput:document.querySelector("[id=search-box]")};console.log(e.searchInput),fetch("https://restcountries.com/v3.1/name/peru?fields=name.official,capital,population,languages,flags.svg").then((function(e){return e.json()})).then((function(e){console.log(e)}))}();
+//# sourceMappingURL=index.dbe437c6.js.map
