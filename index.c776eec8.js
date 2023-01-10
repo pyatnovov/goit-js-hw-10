85a649c76a5b21375c627247795d47490cb4706f
pyatnovov <109892815+pyatnovov@users.noreply.github.com>
2023-01-10 15:00:14 +0000

    Deploying to gh-pages from @ pyatnovov/goit-js-hw-10@1a91717e51911cad870278986d3f1b33e1ceed94 🚀

--- index.c776eec8.js
+++
@@ -1,2 +0,0 @@
-const e={searchInput:document.querySelector("[id=search-box]")};console.log(e.searchInput),fetch("https://restcountries.com/v3.1/name/peru?fields=name.official,capital,population,languages,flags.svg").then((e=>e.json())).then((e=>{console.log(e)}));
-//# sourceMappingURL=index.c776eec8.js.map
--- index.dbe437c6.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={searchInput:document.querySelector("[id=search-box]")};console.log(e.searchInput),fetch("https://restcountries.com/v3.1/name/peru?fields=name.official,capital,population,languages,flags.svg").then((function(e){return e.json()})).then((function(e){console.log(e)}))}();
-//# sourceMappingURL=index.dbe437c6.js.map
